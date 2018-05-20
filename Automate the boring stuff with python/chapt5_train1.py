1766693294156b9fff93c64e59631cca7181a5e9
NHRD <naohisa.harada@gmail.com>
2018-05-20 11:40:35 +0900

    a

---
+++ Automate the boring stuff with python/chapt5_train1.py
@@ -0,0 +1,11 @@
+def display_inventory(inventory):
+    print("Inventry list:")
+    item_total = 0
+    for k in inventory.keys():
+        item_total = item_total + inventory[k]
+        print(str(inventory[k]) + " : " + k)
+    print("Total amount of items: " + str(item_total))
+
+inventory = {"toach":6, "gold":42, "bow":32, "shuriken":23}
+
+display_inventory(inventory)
