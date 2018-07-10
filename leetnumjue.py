6e1378c3a9200f4e40f17ecac672e85abbdffefd
NHRD <naohisa.harada@gmail.com>
2018-07-10 22:24:08 +0900

    a

--- leetnumjue.py
+++ leetnumjue.py
@@ -1,7 +1,18 @@
 class Solution:
-    def numJewelsInStones(self, J, S):
+
+    def __init__(self, juel, stone):
+        self.juel = juel
+        self.stone = stone
+
+    def numJewelsInStones(self):
+        S = self.juel + self.stone
+        return S
         """
         :type J: str
         :type S: str
         :rtype: int
         """
+
+result = Solution(2, 3)
+result2 = result.numJewelsInStones()
+print(result2)
