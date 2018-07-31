e388098a9c4151a5ec13b985aee7cd4eb9e0cf08
NHRD <naohisa.harada@gmail.com>
2018-07-31 21:39:59 +0900

    a

--- web_scraper_test.py
+++ web_scraper_test.py
@@ -4,15 +4,16 @@ import bs4
 
 #this is a comment for test of git push
 
-res = requests.get("https://news.google.com/")
+res = requests.get("https://landing.google.com/sre/book/chapters/foreword.html")
 print(type(res))
 #print(res.status_code)
 soup = bs4.BeautifulSoup(res.text, "html.parser")
 so = soup.find_all("a")
 print(type(so))
 
-print(len(so))
+#print(len(so))
 
+"""
 res = urllib.request.urlopen("https://news.google.com/")
 print(type(res))
 html = res.read()
