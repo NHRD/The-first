d98645f20a27bd328374cab1cbf4afc990d25a99
NHRD <naohisa.harada@gmail.com>
2018-08-02 19:30:10 +0900

    a

---
+++ Find_longest_word_in_dictiaonary.py
@@ -0,0 +1,24 @@
+S = "abppplee"
+D = ["able", "ale", "apple", "bale", "kangaroo"]
+
+print("S:" + S)
+print("D:{}" .format(D))
+
+i = 0
+ok = ""
+l = len(S) - 1
+
+for w in D:
+    j = 0
+    while j < l:
+        if w[0] == S[j]:
+            j += 1
+            w = w[1:]
+        else:
+            j +=1
+    if w == "":
+        if len(ok) < len(D[i]):
+            ok = D[i]
+    i += 1
+
+print(ok)
