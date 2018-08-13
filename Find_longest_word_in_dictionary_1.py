a6e3ee91a28f5887b96301eb6fe0c65ef6a7aadf
NHRD <naohisa.harada@gmail.com>
2018-08-13 16:48:23 +0900

    a

--- Find_longest_word_in_dictionary_1.py
+++ Find_longest_word_in_dictionary_1.py
@@ -1,6 +1,12 @@
 #!/usr/bin/env python
 import collections
 import sys
+import logging
+
+logging.basicConfig(level = logging.DEBUG, format = "%(asctime)s - %(levelname)s - %(message)s")
+logging.debug("program start.")
+
+#logging.disable(logging.DEBUG)
 
 def find_longest_word_in_string(letters, words):
     letter_positions = collections.defaultdict(list)
@@ -17,18 +23,39 @@ def find_longest_word_in_string(letters, words):
         #wordsの中の単語を長さで降順ソート
         pos = 0
         for letter in word:
+            logging.debug("word is {}, letter is {}." .format(word, letter))
             if letter not in letter_positions:
                 break
-        #文字がletter_positionsになければbreak
+            #文字がletter_positionsになければbreak
 
-        possible_positions = [p for p in letter_positions[letter] if p >= pos]
-        if not possible_positions:
-            break
-        pos = possible_positions[0] + 1
+            possible_positions = [p for p in letter_positions[letter] if p >= pos]
+            #pは対象のletterの位置。forループ内の途中のif文がbreakされてもposは加算されるため、
+            #次の文字が判定対象になる。
+            logging.debug("possible_positions are {}." .format(possible_positions))
+            logging.debug("pos is {}." .format(pos))
+            if not possible_positions:
+                break
+            logging.debug("possible_positions[0] is {}." .format(possible_positions[0]))
+            pos = possible_positions[0] + 1
+            #possible_positin[0]はletterの位置を格納したリストの先頭の数字を返す。
+            #↑により、lettersの中で判定対象の文字を現在のletterより後の文字とする。
+            #例：letters = "aaapplle"、word = "apple"の場合、
+            #一周目のループのletterは"a"のため、possible_positions = (0, 1, 2)
+            #次のループのposは0 + 1 = 1
+            #次のループではletterは"p"のため、possible_positions = (3, 4)
+            #次のループのposは3 + 1 = 4
+            #次のループではletterは"p"だが、p >= posより4以上でが条件のため、
+            #possible_positions = (4)
+            
+        #単語に含まれる文字が無くなるまでforが回った場合breakされず、その単語を返す。
         else:
             return word
 
 if __name__ == '__main__':
-    print(subdict(sys.argv[1], sys.argv[2:]))
+    #print(subdict(sys.argv[1], sys.argv[2:]))
+
+    letters = "abppplee"
+    words = ["able", "ale", "apple", "bale", "kangaroo"]
+    print(find_longest_word_in_string(letters, words))
 
 #コマンドラインから直接スクリプトファイルを実行した場合は「__name__という変数（属性）の中に自動的に__main__を代入する」操作が一番はじめにで行われる。
