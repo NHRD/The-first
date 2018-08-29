b2be565c1d34446380a648ef79f81ccbf3676109
NHRD <naohisa.harada@gmail.com>
2018-08-29 21:08:17 +0900

    a

--- Find_longest_word_in_dictionary.py
+++ Find_longest_word_in_dictionary.py
@@ -1,24 +1,24 @@
+def find_longest_word_in_string(letter, words):
+    i = 0
+    ok = ""
+    l = len(letter) - 1
+
+    for w in words:
+        j = 0
+        while j < l:
+            if w[0] == letter[j]:
+                j += 1
+                w = w[1:]
+            else:
+                j +=1
+        if w == "":
+            if len(ok) < len(words[i]):
+                ok = words[i]
+        i += 1
+
+    return(ok)
+
 S = "abppplee"
 D = ["jam", "jamboleeee", "able", "ale", "apple", "bale", "kangaroo"]
-
-print("S:" + S)
-print("D:{}" .format(D))
-
-i = 0
-ok = ""
-l = len(S) - 1
-
-for w in D:
-    j = 0
-    while j < l:
-        if w[0] == S[j]:
-            j += 1
-            w = w[1:]
-        else:
-            j +=1
-    if w == "":
-        if len(ok) < len(D[i]):
-            ok = D[i]
-    i += 1
-
-print(ok)
+result = find_longest_word_in_string(S, D)
+print(result)
